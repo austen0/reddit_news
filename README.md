71a57e6de88275b02b0a2a1704da6521313b152f
Austen Casey <austen@cas3y.com>
2018-04-10 13:38:49 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# reddit_news
+
+A bot to automatically submit news articles to a given subreddit.
+
+## Dependencies
+
+* [logzero](https://pypi.python.org/pypi/logzero/1.5.0)
+* [newsapi](https://pypi.python.org/pypi/newsapi/0.1.1)
+* [PRAW](https://pypi.python.org/pypi/praw/5.4.0)
+* [Unidecode](https://pypi.python.org/pypi/Unidecode/1.0.22)
---
+++ reddit_news.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import config
+import logging
+import logzero
+import praw
+import time
+from newsapi import NewsApiClient
+from unidecode import unidecode
+
+LOGGER = logzero.logger
+
+
+class Reddit(object):
+
+  def __init__(self):
+    self.reddit = praw.Reddit(
+      client_id=config.REDDIT_CLIENT_ID,
+      client_secret=config.REDDIT_CLIENT_SECRET,
+      username=config.REDDIT_USERNAME,
+      password=config.REDDIT_PASSWORD,
+      user_agent='Powered by NewsAPI.org'
+    )
+
+  def submit(self, sub, entries):
+    LOGGER.info('Submitting latest articles to: %s' % sub)
+    subreddit = self.reddit.subreddit(sub)
+    for title, url in entries.iteritems():
+      try:
+        subreddit.submit(
+          title,
+          url=url,
+          resubmit=False,
+          send_replies=False
+        )
+        LOGGER.info('Article submitted: %s' % title)
+      except praw.exceptions.APIException as e:
+        LOGGER.debug(e)
+
+
+class NewsApi(object):
+
+  def __init__(self):
+    self.newsapi = NewsApiClient(api_key=config.NEWSAPI_KEY)
+
+  def getTopNews(self, source):
+    LOGGER.info('Fetching latest news for: %s' % source)
+    articles = dict()
+
+    try:
+      top_headlines = self.newsapi.get_top_headlines(
+        sources=source,
+        language='en'
+      )
+    except Exception as e:
+      LOGGER.error(e)
+
+    for article in top_headlines['articles']:
+      title = unidecode(article['title'])
+      url = article['url']
+      articles[title] = url
+    LOGGER.info('Fetch complete. # of articles: %d' % len(articles))
+    return articles
+
+
+def main():
+  reddit = Reddit()
+  newsapi = NewsApi()
+  logzero.logfile(
+    'reddit_news.log', maxBytes=1e7, backupCount=3, loglevel=logging.INFO)
+
+  while True:
+    LOGGER.info('Update started.')
+    for subreddit, source in config.FEEDS.iteritems():
+      articles = newsapi.getTopNews(source)
+      reddit.submit(subreddit, articles)
+
+    LOGGER.info('Update complete.')
+    time.sleep(config.RUN_FREQUENCY_MINS * 60)
+
+
+if __name__ == '__main__':
+  main()
